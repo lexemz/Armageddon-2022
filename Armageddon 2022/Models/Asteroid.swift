093155b03d26c2dfa9015d227f275ae4400eef90
Igor Buzykin <lexemz@gmail.com>
2022-05-28 22:26:59 +0300

    Расширение модели mock методом

--- Armageddon 2022/Models/Asteroid.swift
+++ Armageddon 2022/Models/Asteroid.swift
@@ -14,3 +14,24 @@ struct Asteroid {
     let arrivalDistance: Double
     let isDangerous: Bool
 }
+
+extension Asteroid {
+    static func generateMock() -> [Self] {
+        [
+            Asteroid(
+                name: "QQ 2022",
+                diametr: 500,
+                arrivalDate: "06-09-2022",
+                arrivalDistance: 3321,
+                isDangerous: true
+            ),
+            Asteroid(
+                name: "QC 2021",
+                diametr: 300,
+                arrivalDate: "20-06-2022",
+                arrivalDistance: 112,
+                isDangerous: false
+            )
+        ]
+    }
+}
