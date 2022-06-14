62d7a0f1306fd161fcd42a7f70d6043f402843ef
Igor Buzykin <lexemz@gmail.com>
2022-06-14 23:27:00 +0300

    Обновлена главная модель

--- Armageddon 2022/Models/Asteroid.swift
+++ Armageddon 2022/Models/Asteroid.swift
@@ -10,9 +10,14 @@ import Foundation
 struct Asteroid {
     let name: String
     let diametr: Double
-    let arrivalDate: Date
-    let arrivalDistance: Double
+    let arrivalDate: Date?
+    let arrivalDistance: AsteroidArrivarDistance
     let isDangerous: Bool
+}
+
+struct AsteroidArrivarDistance {
+    let kilometers: Double
+    let lunar: Double
 }
 
 extension Asteroid {
@@ -22,17 +27,30 @@ extension Asteroid {
                 name: "2021 FQ",
                 diametr: 250,
                 arrivalDate: Date(timeIntervalSince1970: 1655082000),
-                arrivalDistance: 3321,
+                arrivalDistance: AsteroidArrivarDistance(kilometers: 132, lunar: 324),
                 isDangerous: true
             ),
             Asteroid(
                 name: "2022 QQ",
                 diametr: 85,
                 arrivalDate: Date(timeIntervalSince1970: 1655168400),
-                arrivalDistance: 112,
+                arrivalDistance: AsteroidArrivarDistance(kilometers: 132, lunar: 324),
                 isDangerous: false
             )
         ]
     }
     
+//    init(nearEarthObject: NearEarthObject) {
+//        let maxDiametr = nearEarthObject.estimatedDiameter.meters.estimatedDiameterMax
+//        let minDiametr = nearEarthObject.estimatedDiameter.meters.estimatedDiameterMin
+//        let avgDiametr = (maxDiametr + minDiametr) / 2
+//        
+//        self.init(
+//            name: nearEarthObject.name,
+//            diametr: avgDiametr,
+//            arrivalDate: nearEarthObject.closeApproachData.first?.closeApproachDate,
+//            arrivalDistance: nearEarthObject.,
+//            isDangerous: nearEarthObject.isPotentiallyHazardousAsteroid
+//        )
+//    }
 }
--- Armageddon 2022/Views/AsteroidCellViewModel.swift
+++ Armageddon 2022/Views/AsteroidCellViewModel.swift
@@ -11,50 +11,54 @@ class AsteroidCellViewModel: AsteroidCellViewModelType {
     var isDangerous: Bool {
         asteroid.isDangerous
     }
-    
+
     var diametr: Double {
         asteroid.diametr
     }
-    
+
     var name: String {
         asteroid.name
     }
-    
+
     var diametrText: String {
         "Диаметр: \(asteroid.diametr)м"
     }
-    
+
     var arrivalDateText: String {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ru_RU")
         dateFormatter.dateFormat = "d MMM y"
-        return "Подлетает: \(dateFormatter.string(from: asteroid.arrivalDate))"
+        if let arrivalDate = asteroid.arrivalDate {
+            return "Подлетает: \(dateFormatter.string(from: arrivalDate))"
+        } else {
+            return "Подлетает: неизвстно когда"
+        }
     }
-    
+
     var arrivalDistanceText: String {
-        "на расстояние \(asteroid.arrivalDistance) км"
+        "на расстояние \(asteroid.arrivalDistance.kilometers) км"
     }
-    
+
     var dangerStatusTextComponents: [String: String] {
         [
             "head": "Оценка: ",
-            "status": isDangerous ? "опасен" : "не опасен"
+            "status": isDangerous ? "опасен" : "не опасен",
         ]
     }
-    
+
     private let asteroid: Asteroid
     private let indexPath: IndexPath
     private let deleagte: AsteroidCellViewModelDelegate
-    
+
     init(asteroid: Asteroid,
          indexPath: IndexPath,
-         delegate: AsteroidCellViewModelDelegate
-    ) {
+         delegate: AsteroidCellViewModelDelegate)
+    {
         self.asteroid = asteroid
         self.indexPath = indexPath
-        self.deleagte = delegate
+        deleagte = delegate
     }
-    
+
     func destroyButtonIsPressed() {
         deleagte.destroyButtonIsPressed(at: indexPath)
     }
